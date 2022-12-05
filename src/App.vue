76e5ff6710f252d1dc7d7fd0e0747ec206ae455a
GaliSW <gary850204@gmail.com>
2022-12-06 01:21:56 +0800

    add->import popup.vue && trigger function

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <template>
     <header>
         <router-link to="/books">
-            <i class="fa-solid fa-chevron-left" v-if="route.path !== '/books'"></i>
+            <i
+                class="fa-solid fa-chevron-left"
+                v-if="route.path !== '/books'"
+            ></i>
         </router-link>
         <h1>{{ book.title }}</h1>
-        <i class="fa-solid fa-plus" v-if="route.path == '/books'"></i>
-        <i class="fa-sharp fa-solid fa-file-pen" v-else></i>
+        <i
+            class="fa-solid fa-plus"
+            @click="popup(1)"
+            v-if="route.path == '/books'"
+        ></i>
+        <i class="fa-sharp fa-solid fa-file-pen" @click="popup(2)" v-else></i>
     </header>
     <router-view></router-view>
-    <AddPop></AddPop>
+    <Pop v-show="pop === 1 || pop === 2" :pop="pop" @close-pop="closePop"></Pop>
 </template>
 
 <script setup>
-import AddPop from "@/components/AddBook.vue"
-import EditPop from "@/components/EditBook.vue"
-import { bookStore } from "@/store/book"
-import { useRoute } from 'vue-router'
-const route = useRoute()
-const book = bookStore()
+import Pop from "@/components/Popup.vue";
+import { ref } from "vue";
+import { bookStore } from "@/store/book";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const book = bookStore();
+const pop = ref(0);
 
+const popup = (type) => {
+    pop.value = type;
+};
+
+const closePop = () => {
+    pop.value = 0;
+};
 </script>
 <style lang="scss">
 @import "@/assets/scss/style";
