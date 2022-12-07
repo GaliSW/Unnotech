5b3e3466524f54f5aef3d70b6ca29b5983d4f686
GaliSW <gary850204@gmail.com>
2022-12-07 15:53:35 +0800

    add -> navigation guard

--- src/pages/BookDetail.vue
+++ src/pages/BookDetail.vue
@@ -1,7 +1,14 @@
 <template>
     <section id="book_detail">
         <div class="thumbnail">
-            <img :src="book.bookInfo.image ? book.bookInfo.image : 'https://img.onl/f7yYXv'" alt="">
+            <img
+                :src="
+                    book.bookInfo.image
+                        ? book.bookInfo.image
+                        : 'https://img.onl/N7lQ9o'
+                "
+                alt=""
+            />
         </div>
         <div>
             <h3>名稱</h3>
@@ -13,19 +20,24 @@
         </div>
         <div class="description">
             <h3>備註</h3>
-            <p>{{ book.bookInfo.description }}
-            </p>
+            <p>{{ book.bookInfo.description }}</p>
         </div>
     </section>
 </template>
 <script setup>
-import { onMounted, ref } from 'vue';
-import { getBookApi } from '@/api/api';
-import { useRoute } from 'vue-router'
+import { onMounted, ref } from "vue";
+import { getBookApi } from "@/api/api";
+import { useRoute, useRouter } from "vue-router";
 import { bookStore } from "@/store/book";
 
 const book = bookStore();
 const route = useRoute();
+const router = useRouter();
+
+router.beforeEach(async (to, from) => {
+    book.bookInfo.image = "";
+});
+
 onMounted(() => {
     getBookApi(route.params.id).then((res) => {
         book.bookInfo.title = res.data.title;
@@ -33,6 +45,6 @@ onMounted(() => {
         book.bookInfo.author = res.data.author;
         book.bookInfo.description = res.data.description;
         book.bookInfo.image = res.data.image;
-    })
-})
+    });
+});
 </script>
